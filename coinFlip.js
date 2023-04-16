d9f3dc285fd0c73971434c25f42fe20ca413681f
Cory Musick <cory.musick37@gmail.com>
2023-04-16 14:39:35 -0500

    fix: completed flipCoin function and linked to button in index html

--- coinFlip.js
+++ coinFlip.js
@@ -1,7 +1,6 @@
-const result = Math.floor(Math.random() * 2)
+// This functions randomly generates either 0 or 1 and uses a ternary operatator
+// to alert either "heads" or "tails" to simulate a coin toss
 
-if(result = 1){
-  alert("heads")
-}else{
-  alert("false")
+function flipCoin() {
+  Math.floor(Math.random() * 2) ? alert("heads") : alert("tails");
 }
